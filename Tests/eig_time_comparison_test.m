45e5b27b2990f4be44f8cb039177a1c5caa45cd5
MilaNedic <49908385+MilaNedic@users.noreply.github.com>
2024-10-14 12:53:17 +0200

    Add files via upload

--- Tests/eig_time_comparison_test.m
+++ Tests/eig_time_comparison_test.m
@@ -1,7 +1,9 @@
 % Test script for calculating the average time for computing eigenvalues
 % of a random matrix of size n x n over m iterations
-n = 10;
-m = 100;
+n = 100;
+m = 10;
 [cpu_time, gpu_time] = eig_time_comparison(n, m);
+ratio = cpu_time/gpu_time;
 fprintf('Average CPU time: %f seconds\n', cpu_time);
 fprintf('Average GPU time: %f seconds\n', gpu_time);
+fprintf('cpu_time/gpu_time: %f', ratio);
